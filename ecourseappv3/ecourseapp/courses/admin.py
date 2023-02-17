b2b56ce92ff88f3cb9b6fd889aa21581e86784e1
huynhthiphuongtram <1951052208Tram@ou.edu.vn>
2023-02-17 19:54:35 +0700

    code phase three

--- ecourseappv3/ecourseapp/courses/admin.py
+++ ecourseappv3/ecourseapp/courses/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category, Course
+
+admin.site.register(Category)
+admin.site.register(Course)
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["id","subject","created_date","active"]
+
 
 # Register your models here.
